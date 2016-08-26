938b705d8a8146623946b9f69361da0474924773
James Williams <jwilliams@pivotal.io>
2016-08-26 15:49:27 -0400

    ddd

--- concourse/scripts/zero-downtime-push.sh
+++ concourse/scripts/zero-downtime-push.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 cf login -a https://api.local2.pcfdev.io --skip-ssl-validation -u admin -p admin -o pcfdev-org -s pcfdev-space
-cf zero-downtime-push attendees-citi -f manifest.yml -p pcf-ers-demo.jar
+cf zero-downtime-push attendees-citi -f output/manifest.yml -p output/pcf-ers-demo.jar
