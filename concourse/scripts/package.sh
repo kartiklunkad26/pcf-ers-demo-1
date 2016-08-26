f15ad6cee362f30426ccbcfece81984f11344735
James Williams <jwilliams@pivotal.io>
2016-08-26 15:22:12 -0400

    ddd

--- concourse/scripts/package.sh
+++ concourse/scripts/package.sh
@@ -4,10 +4,10 @@ echo "building jar binary"
 source-code/mvnw -f pom.xml package -DskipTests
 
 echo "copying jar binary to output directory"
-cp source-code/target/*.jar ../output
+cp source-code/target/*.jar ./output
 
 echo "copying concourse scripts to output directory"
-cp source-code/concourse/scripts/* ../output
+cp source-code/concourse/scripts/* ./output
 
 echo "copying cf manifest file to output directory"
-cp source-code/manifest.yml ../output
+cp source-code/manifest.yml ./output
