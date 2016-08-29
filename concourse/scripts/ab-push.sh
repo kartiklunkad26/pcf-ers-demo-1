cbde843adbf40557125a3bf08d52715d34c70749
James Williams <jwilliams@pivotal.io>
2016-08-29 08:04:04 -0400

    streamlined sw repos

--- concourse/scripts/ab-push.sh
+++ concourse/scripts/ab-push.sh
@@ -2,7 +2,8 @@
 
 cf login -a https://api.local2.pcfdev.io --skip-ssl-validation -u admin -p admin -o pcfdev-org -s pcfdev-space
 
-cf push attendees-citi-a -f output/manifest.yml -p output/pcf-ers-demo-a.jar
-cf map-route attendees-citi-a local2.pcfdev.io --hostname attendees-citi-ab
-cf push attendees-citi-b -f output/manifest.yml -p output/pcf-ers-demo-b.jar
-cf map-route attendees-citi-b local2.pcfdev.io --hostname attendees-citi-ab
+cf push attendees-a -f manifest.yml -p pcf-ers-demo-a.jar
+cf map-route attendees-a local2.pcfdev.io --hostname attendees-ab
+
+cf push attendees-b -f manifest.yml -p pcf-ers-demo-b.jar
+cf map-route attendees-b local2.pcfdev.io --hostname attendees-ab
