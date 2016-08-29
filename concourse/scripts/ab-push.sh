3c0bf705c490fcab3d73e2a83c320fcd785ecf91
James Williams <jwilliams@pivotal.io>
2016-08-29 11:18:46 -0400

    fixing AB release

--- concourse/scripts/ab-push.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-cf login -a https://api.local2.pcfdev.io --skip-ssl-validation -u admin -p admin -o pcfdev-org -s pcfdev-space
-
-cf push attendees-a -f manifest.yml -p pcf-ers-demo-a.jar
-cf map-route attendees-a local2.pcfdev.io --hostname attendees-ab
-
-cf push attendees-b -f manifest.yml -p pcf-ers-demo-b.jar
-cf map-route attendees-b local2.pcfdev.io --hostname attendees-ab
--- concourse/scripts/package.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-pcf-ers-demo/mvnw -f pcf-ers-demo/pom.xml package -DskipTests || { echo 'error building jar binary' ; exit 1; }
-cp pcf-ers-demo/target/*.jar ./output || { echo 'error copying jar to output directory' ; exit 1; }
-cp pcf-ers-demo/concourse/scripts/* ./output || { echo 'error copying scripts to output directory' ; exit 1; }
-cp pcf-ers-demo/manifest.yml ./output || { echo 'error manifest to output directory' ; exit 1; }
-chmod 755 ./output/*
